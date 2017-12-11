34bfb4aef14d8913a08fb2f1ecb497f7b0d57370
Pradeep Kumar <pradeepkumar@mstsolutions.com>
2017-12-11 13:52:56 +0530

    thank you page

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
+	console.log('path = ', path.join(__dirname + '/index.html'));
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
