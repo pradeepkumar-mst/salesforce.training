be823a03724d76a721f136d646e77d86e482d989
Pradeep Kumar <pradeepkumar@mstsolutions.com>
2017-12-11 13:43:04 +0530

    thank you page

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
+var appExpress = express();
 var port = process.env.PORT || 3000;
-var app = express.createServer();
+var app = appExpress.createServer();
 
 app.get('/', function(request, response) {
     response.sendfile(__dirname + '/index.html');
