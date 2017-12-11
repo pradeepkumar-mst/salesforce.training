af1ab4d10ad2b00aa5607dca99cfc43f1f0ebe7a
Pradeep Kumar <pradeepkumar@mstsolutions.com>
2017-12-11 13:47:01 +0530

    thank you page

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var express = require('express');
-var appExpress = express();
-var port = process.env.PORT || 3000;
-var app = appExpress.createServer();
+var app = express();
+var path = require('path');
 
-app.get('/', function(request, response) {
-    response.sendfile(__dirname + '/index.html');
-}).configure(function() {
-    app.use('/images', express.static(__dirname + '/images'));
-}).listen(port);
+// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(8080);
