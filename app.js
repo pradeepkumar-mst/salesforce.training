88669e272eaed6eb7d17d7aaaef5ea9521124966
Pradeep Kumar <pradeepkumar@mstsolutions.com>
2017-12-26 13:43:27 +0530

    package json changes

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+var port = process.env.PORT || 8080; // set our port
+
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
 	console.log('path = ', path.join(__dirname + '/index.html'));
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(8080);
+app.listen(port);
+console.log('Magic happens on port ' + port); 			// shoutout to the user
+exports = module.exports = app; 						// expose app
